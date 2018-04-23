2cc2241c1a66d495acba8e43b7d57fac0511eee1
Martin Sarsini <martin@apphancer.com>
2018-04-23 16:31:48 +0200

    Initial commit

---
+++ content-script.js
@@ -0,0 +1,40 @@
+var calendar_grid_selector = 'div[role="grid"]';
+var body = document.querySelector('body');
+var calendar_grid = document.querySelectorAll(calendar_grid_selector);
+
+var disable_scroll = function () {
+    for (var live_selector of document.querySelectorAll(calendar_grid_selector)) {
+        live_selector.addEventListener('DOMMouseScroll', function (e) {
+            if (e.target.id == 'el') return;
+            e.preventDefault();
+            e.stopPropagation();
+        });
+    }
+};
+
+var mutation_breaks_scroll_blocker = function (mutation) {
+    if (mutation.attributeName && mutation.attributeName == 'data-viewfamily') {
+        if (body.getAttribute('data-viewfamily') == 'EVENT')
+            return true;
+    }
+};
+
+var calendar_observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutation) {
+        if (mutation_breaks_scroll_blocker(mutation)) {
+            disable_scroll();
+        }
+    });
+});
+
+var observe_if_calendar_available = function () {
+    if (!calendar_grid) {
+        alert('calendar_grid is not yet showing');
+        window.setTimeout(observe_if_calendar_available, 500);
+        return;
+    }
+    calendar_observer.observe(body, {attributes: true});
+};
+
+disable_scroll();
+observe_if_calendar_available();
